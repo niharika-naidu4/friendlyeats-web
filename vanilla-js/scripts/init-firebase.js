0fcaab4dc6f127306bf4df27adc15c48bf7548bf
niharika-naidu4 <immadiniharikanaidu@gmail.com>
2025-04-10 03:17:11 -0700

    Add Firebase Hosting GitHub workflow

---
+++ vanilla-js/scripts/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "green-mercury-456409-c0.firebaseapp.com",
+  projectId: "green-mercury-456409-c0",
+  storageBucket: "green-mercury-456409-c0.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
